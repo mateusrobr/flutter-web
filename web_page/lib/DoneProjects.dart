543bd60a558173f14886fcf114b7a612b8497222
Luiz Mateus <103285835+mateusrobr@users.noreply.github.com>
2023-08-25 11:28:13 -0300

    Update DoneProjects.dart

--- web_page/lib/DoneProjects.dart
+++ web_page/lib/DoneProjects.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:web_page/white_container.dart';
 
 
 class DoneProjects extends StatelessWidget{
@@ -11,6 +12,7 @@ class DoneProjects extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -21,26 +23,34 @@ class DoneProjects extends StatelessWidget{
           ),
         ),
       ),
-      body: Container(
-        child: Column(
-          children: doneProjects.entries.map((element) {
-            return Column(
-              children: [
-                Text(
-                  element.key,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
+      body: WhiteContainer().getWhiteContainer(context, Column(
+        children: doneProjects.entries.map((element) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                child: Container( 
+                  width: size.width,
+                  color: Color.fromARGB(255, 111, 182, 240),
+                  child: Center(
+                    child: Text(
+                      element.key,
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold),
+                        selectionColor: Colors.black,),
                   ),
                 ),
-                Container(
-                  child: Text(element.value),
-                )
-              ],
-            );
-          }).toList(),
-        ),
-      ),
+              ),
+              Container(
+                
+                color: const Color.fromARGB(255, 0, 86, 156),
+                child: Text(element.value),
+              )
+            ],
+          );
+        }).toList(),
+      ))
     );
   }
 }
